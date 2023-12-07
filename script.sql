33db55d205c8ba529de28f9f2142b107ba04571f
Emily Ferreira <125586313+emyferreira@users.noreply.github.com>
2023-12-07 14:41:08 -0300

    tirando a tabela métrica

--- script.sql
+++ script.sql
@@ -1,27 +1,24 @@
-CREATE DATABASE IF NOT EXISTS  DesafioJava;
-USE DesafioJava;
-
 CREATE USER 'DesafioJava'@'%' IDENTIFIED BY 'deafiojava2023';
 GRANT ALL PRIVILEGES ON DesafioJava.* TO 'DesafioJava'@'%';
 FLUSH PRIVILEGES;
 
-CREATE TABLE Metrica(
-	idMetrica INT PRIMARY KEY AUTO_INCREMENT,
-	limiteMaximo float
-);
+DROP DATABASE IF EXISTS DesafioJava;
+CREATE DATABASE IF NOT EXISTS  DesafioJava;
+USE DesafioJava;
 
 CREATE TABLE Componente(
 	idComponente INT PRIMARY KEY AUTO_INCREMENT,
-    nomeComponente VARCHAR(10),
+    nomeComponente VARCHAR(30),
 	unidadeMedida VARCHAR(5),
-    fkMetrica INT,
-	CONSTRAINT fkMetrica FOREIGN KEY (fkMetrica) REFERENCES Metrica(idMetrica)
+    metricaComponente FLOAT
 );
 
 CREATE TABLE Registro (
 	idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-    registro float,
+    registro FLOAT,
 	dateDado DATETIME,
 	fkComponente INT,
 	CONSTRAINT fkComponente FOREIGN KEY (fkComponente) REFERENCES Componente(idComponente)
 );
+
+select * from Componente;
